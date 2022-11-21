06024d9434ed4328c9435c15f4c169a724adf659
hamzamu6 <71731160+MD-Hamza@users.noreply.github.com>
2022-11-20 19:10:53 -0500

    JavaFX starter

---
+++ src/ImageRecognition.java
@@ -0,0 +1,18 @@
+package src;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ImageRecognition extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.show();
+    }
+
+    /**
+     * Main method
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
