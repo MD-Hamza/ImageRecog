831d62826bd83a776a2cbae609c63d39a79cd92c
Karanvir1729 <86384415+Karanvir1729@users.noreply.github.com>
2022-11-25 03:01:36 -0500

    [DEV 1.7] Created a class for converting image files to an arraylist of arrays that has the RGB vals of the images.

---
+++ src/ImagesToMap.java
@@ -0,0 +1,42 @@
+package src;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ImagesToMap {
+    private ArrayList rgbVals;
+
+    public void setMapofImage(String fileName) {
+        Image image = new Image(fileName);
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+
+        PixelReader pixelReader = image.getPixelReader();
+        double red = 0;
+        double green = 0;
+        double blue = 0;
+        int n = 0;
+        for (int x = 0; x < image.getWidth(); x += 50) {
+            for (int y = 0; y < image.getHeight(); y += 50) {
+                red += pixelReader.getColor(x, y).getRed();
+                green += pixelReader.getColor(x, y).getGreen();
+                blue += pixelReader.getColor(x, y).getBlue();
+                n += 1;
+            }
+        }
+        red = red / n;
+        green = green / n;
+        blue = blue / n;
+
+        double[] col = new double[]{red, blue, green};
+        rgbVals.add(col);
+    }
+
+    public ArrayList<Arrays> getRgbVals() {
+        return rgbVals;
+    }
+}
