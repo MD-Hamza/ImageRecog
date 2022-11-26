2e1521ed71af211f2db545a9c8b13e095a7a0b0f
Nipun <nsaini534@gmail.comm>
2022-11-25 21:49:20 -0500

    [DEV-1.4]. Set up the overall structure of the application. Implemented 2 buttons, one for each command that prompt up the file dialog for the user to choose singular or multiple images for whatever command they choose to apply. Converted each image selected to BufferedImage making it easier to work with for the rest of the application.

--- src/ImageRecognition.java
+++ src/ImageRecognition.java
@@ -1,12 +1,87 @@
 package src;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageRecognition extends Application {
+    private final FileChooser fileChooser = new FileChooser();
+    private final FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.gif", "*.jpeg");
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Button classify = new Button("CLASSIFY");
+        Button upload = new Button("UPLOAD");
+        VBox v = new VBox();
+        v.getChildren().add(classify);
+        v.getChildren().add(upload);
+        Scene sc = new Scene(v, 800, 800);
+        primaryStage.setScene(sc);
         primaryStage.show();
+        classify.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent e) {
+                        if (fileChooser.getExtensionFilters().size() == 0){
+                            fileChooser.getExtensionFilters().add(filter);
+                        }
+                        ArrayList<BufferedImage> b = new ArrayList<>();
+                        List<File> hold = fileChooser.showOpenMultipleDialog(primaryStage);
+                        if (hold != null){
+                            for (File f: hold){
+                                try {
+                                    BufferedImage img = ImageIO.read(f);
+                                    b.add(img);
+                                } catch (IOException ex) {
+                                    throw new RuntimeException(ex);
+                                }
+                            }
+                        }
+                        else{
+                            return;
+                        }
+                        ClassifyCommand c = new ClassifyCommand(b);
+
+                    }
+                }
+        );
+        upload.setOnAction(
+                new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent actionEvent) {
+                        if (fileChooser.getExtensionFilters().size() == 0){
+                            fileChooser.getExtensionFilters().add(filter);
+                        }
+                        ArrayList<BufferedImage> b = new ArrayList<>();
+                        List<File> hold = fileChooser.showOpenMultipleDialog(primaryStage);
+                        if (hold != null){
+                            for (File f: hold){
+                                try {
+                                    BufferedImage img = ImageIO.read(f);
+                                    b.add(img);
+                                } catch (IOException ex) {
+                                    throw new RuntimeException(ex);
+                                }
+                            }
+                        }
+                        else{
+                            return;
+                        }
+                        UploadCommand c = new UploadCommand(b);
+                    }
+                }
+        );
+
     }
 
     /**
