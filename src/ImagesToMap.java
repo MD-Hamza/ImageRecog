8fad554d29a0d2a63b6ff55199832a36c237aa10
Karanvir1729 <86384415+Karanvir1729@users.noreply.github.com>
2022-11-26 03:02:58 -0500

    [DEV 1.7] Divided the image pixels into 25 sections to get average values of colours from all over the image. Had to fix some bugs in the previous commit that were allowing 0 division. However, now with the added test I have more trust in the program.

--- src/ImagesToMap.java
+++ src/ImagesToMap.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  * ImageToMap is an observer class of the image data.
  */
 public class ImagesToMap {
-    private ArrayList<ArrayList<Double>> rgbVals;
+    private ArrayList<ArrayList<Double>> rgbVals = new ArrayList<>();
 
     /**
      * Initializes values to the rgbVals arraylist that maps the pixels of the image to the RGB values.
@@ -27,11 +27,11 @@ public class ImagesToMap {
         double blue = 0;
         int n = 0;
 
-        for (int x = 0; x < image.getWidth() / 25; x += 1) {
-            for (int y = 0; y < image.getHeight() / 25; y += 1) {
-                red += pixelReader.getColor(x, y).getRed();
-                green += pixelReader.getColor(x, y).getGreen();
-                blue += pixelReader.getColor(x, y).getBlue();
+        for (int x = 0; x < image.getWidth()/25 ; x += 1) {
+            for (int y = 0; y < image.getHeight()/25; y += 1) {
+                red += pixelReader.getColor(x, y).getRed()*255;
+                green += pixelReader.getColor(x, y).getGreen()*255;
+                blue += pixelReader.getColor(x, y).getBlue()*255;
                 n += 1;
             }
         }
@@ -49,22 +49,23 @@ public class ImagesToMap {
             red = 0;
             green = 0;
             blue = 0;
-            n = 0;
+            int m = 0;
 
-            for (int x = (int) (image.getWidth()*i / 25); x < image.getWidth() *i/ 25; x += 1) {
-                for (int y = (int) (image.getHeight()*i / 25); y < image.getHeight() *i/ 25; y += 1) {
-                    red += pixelReader.getColor(x, y).getRed();
-                    green += pixelReader.getColor(x, y).getGreen();
-                    blue += pixelReader.getColor(x, y).getBlue();
-                    n += 1;
+            for (int x = (int) (image.getWidth() *(i-1)/ 25); x < image.getWidth() *i/ 25; x += 1) {
+                for (int y = (int) (image.getHeight() *(i-1)/ 25); y < image.getHeight() *i/ 25; y += 1) {
+                    red += pixelReader.getColor(x, y).getRed()*255;
+                    green += pixelReader.getColor(x, y).getGreen()*255;
+                    blue += pixelReader.getColor(x, y).getBlue()*255;
+                    m += 1;
                 }
             }
-            red = red / n;
-            green = green / n;
-            blue = blue / n;
+            red = red / m;
+            green = green / m;
+            blue = blue / m;
 
             col = new ArrayList<>();
             col.add(red);
+            //System.out.println(m);
             col.add(green);
             col.add(blue);
             rgbVals.add(col);
@@ -75,7 +76,7 @@ public class ImagesToMap {
 
     /**
      * Get the pixel to image map.
-     * @return rgbVals. The arraylist of pixels in the RGB form as stated earlier.
+     * @return rgbVals. The RGB values of the pixels of the image.
      */
     public ArrayList<ArrayList<Double>> getRgbVals() {
         return rgbVals;
