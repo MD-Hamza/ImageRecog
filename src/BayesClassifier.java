d09bf305dce1eea86a98f8dc449e3302617d26d7
Nipun <76672325+ImOhda@users.noreply.github.com>
2022-12-07 02:39:44 -0500

    [DEV 1.4]- Successfully implemented the Command Center. User is able to classify and upload images at will.

--- src/BayesClassifier.java
+++ src/BayesClassifier.java
@@ -1,14 +1,33 @@
 package src;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.HashMap;
 
 public class BayesClassifier {
-    private String output;
-    private BufferedImage img2;
-    public BayesClassifier(BufferedImage img){
-        this.img2 = img;
+    private HashMap<String, String> result;
+
+    public String getResult() {
+        return result.get(0);
     }
-    public void classify(){
-        throw new UnsupportedOperationException();
+
+    public void classify(BufferedImage img, String modelName) throws IOException {
+        File mainFolder = new File("imageFolder");
+        if (!mainFolder.exists()){
+            mainFolder.mkdirs();
+        }
+        ImagesToMap d = new ImagesToMap();
+        DataVisitor v = new DataVisitor();
+        File outputfile = new File("image.png");
+        ImageIO.write(img, "png", outputfile);
+        d.setMapofImage("image.png");
+        v.clear( "imageFolder\\image.txt");
+        ImageData i = new ImageData(d.getRgbVals(), "imageFolder\\image.txt");
+        v.visitImage(i);
+        Classify classifier = new Classify("./" + modelName, "imageFolder");
+        this.result = classifier.classify();
     }
 }
--- src/ClassifyCommand.java
+++ src/ClassifyCommand.java
@@ -1,18 +1,25 @@
 package src;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class ClassifyCommand implements Command{
     private BufferedImage img;
-    // private Receiver bayesClassifier;
+    private String modelName;
+    public BayesClassifier reciever = new BayesClassifier();
 
-    public ClassifyCommand(BufferedImage img2){
+    public ClassifyCommand(BufferedImage img2, String folderName) throws IOException {
         this.img = img2;
+        this.modelName = folderName;
         execute();
     }
 
     @Override
-    public void execute() {
-
+    public void execute() throws IOException {
+        reciever.classify(this.img, this.modelName);
     }
 }
